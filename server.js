0c89f9b62012ea97028dd1926818691672310665
Srija <118525729+srija-22@users.noreply.github.com>
2023-01-29 17:22:15 +0000

    setup logger middleware

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
-const express = require('express');
-const { establishDbConnection, accessDb } = require('./database')
+const express = require("express");
+const { establishDbConnection, accessDb } = require("./database");
 
+// start app
 const app = express();
 
-establishDbConnection()
-  .then(() => {
-    app.listen(process.env.APP_PORT || 3000, () =>
-      console.log(`Server running on port: ${process.env.APP_PORT || 3000}`)
-    );
-  })
+// access control headers
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+// logger middleware
+app.use((req, res, next) => {
+  console.log({
+    method: req.method,
+    url: req.url,
+    status: res.statusCode,
+  });
+
+  next();
+});
+
+
+
+establishDbConnection().then(() => {
+  app.listen(process.env.APP_PORT || 3000, () =>
+    console.log(`Server running on port: ${process.env.APP_PORT || 3000}`)
+  );
+});
