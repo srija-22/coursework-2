62b34cbd7c155448267053ed34ba7d781b4a09f3
Srija <118525729+srija-22@users.noreply.github.com>
2023-01-29 17:32:53 +0000

    Update server.js

--- server.js
+++ server.js
@@ -34,3 +34,65 @@ establishDbConnection().then(() => {
     console.log(`Server running on port: ${process.env.APP_PORT || 3000}`)
   );
 });
+
+const decrementSpaces = async (lessonId, spacesAmount) => {
+  const database = await accessDb();
+  const lessons = database.collection("lesson");
+
+  try {
+    const result = await lessons.findOneAndUpdate(
+      { _id: ObjectId(lessonId) },
+      { $inc: { spaces: -spacesAmount } }
+    );
+  } catch (err) {
+    console.error("Error while decrementing spaces:", err);
+  }
+};
+
+// GET route that returns all the lessons
+app.get("/all-lessons", async (req, res, next) => {
+  try {
+    const searchTerm = req.query.search || "";
+    const query = searchTerm
+      ? {
+          $or: [
+            { subject: { $regex: searchTerm, $options: "i" } },
+            { location: { $regex: searchTerm, $options: "i" } },
+          ],
+        }
+      : {};
+
+    const db = await accessDb();
+    const lessons = db.collection("lesson");
+    const lessonList = await lessons.find(query).toArray();
+
+    res.send(lessonList);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// POST route that saves a new order to the ‘order’ collection
+app.post("/create-order", async (req, res, next) => {
+  try {
+    const orderData = req.body;
+    const database = await accessDb();
+    const orders = database.collection("order");
+
+    const newOrder = await orders.insertOne(orderData);
+
+    res.send(newOrder);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// PUT route that updates the number of available spaces in the ‘lesson’ collection after an order is submitted
+app.put("/update-lesson/:id", async (req, res) => {
+  const lessonId = req.params.id;
+  const spacesAmount = req.body.spaces;
+
+  await decrementSpaces(lessonId, spacesAmount);
+
+  res.send("lesson spaces has been updated");
+});
